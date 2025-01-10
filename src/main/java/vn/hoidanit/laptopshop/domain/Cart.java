b99c8a3c63a862ade06e308114cc8bd6d710024d
ThanhChuong <thanhchuong1412@gmail.com>
2025-01-11 01:26:48 +0700

    them data product vao gio hang

---
+++ src/main/java/vn/hoidanit/laptopshop/domain/Cart.java
@@ -0,0 +1,48 @@
+package vn.hoidanit.laptopshop.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+
+@Entity
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Min(value = 0)
+    private int sum;
+    @OneToOne()
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSum() {
+        return sum;
+    }
+
+    public void setSum(int sum) {
+        this.sum = sum;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+}
--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -5,15 +5,27 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.CartDetailRepository;
+import vn.hoidanit.laptopshop.repository.CartRepository;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
 
 @Service
 public class ProductService {
      private final ProductRepository productRepository;
+     private final CartDetailRepository cartDetailRepository;
+     private final CartRepository cartRepository;
+     private final UserService userService;
 
-     public ProductService(ProductRepository productRepository) {
+     public ProductService(ProductRepository productRepository, CartRepository cartRepository,
+               CartDetailRepository cartDetailRepository, UserService userService) {
           this.productRepository = productRepository;
+          this.cartDetailRepository = cartDetailRepository;
+          this.cartRepository = cartRepository;
+          this.userService = userService;
      }
 
      public Product handleSaveProduct(Product product) {
@@ -23,10 +35,44 @@ public class ProductService {
      public List<Product> getAllProducts() {
           return this.productRepository.findAll();
      }
-     public Optional<Product> getProductByid(long id){
-          return  this.productRepository.findById(id);
+
+     public Optional<Product> getProductByid(long id) {
+          return this.productRepository.findById(id);
      }
-     public void deleteProduct(long id){
-           this.productRepository.deleteById(id);
+
+     public void deleteProduct(long id) {
+          this.productRepository.deleteById(id);
+     }
+
+     public void handleAddProductToCart(String email, long id) {
+          // check user đã có cart hay chưa ? nếu chưa -> tạo mới
+          User user = this.userService.getUserByEmail(email);
+          if (user != null) {
+               // check user đã có cart hay chưa ? nếu chưa -> tạo mới
+               Cart cart = this.cartRepository.findByUser(user);
+               if (cart == null) {
+                    // create card
+                    Cart otherCart = new Cart();
+                    otherCart.setUser(user);
+                    otherCart.setSum(1);
+
+                    cart = this.cartRepository.save(otherCart);
+               }
+               // save cart_detail
+               // de lưu đc thì cần đối tượng product
+               Optional<Product> productOptional = this.productRepository.findById(id);
+               if (productOptional.isPresent()) {
+                    Product reProduct = productOptional.get();
+
+                    CartDetail cartDetail = new CartDetail();
+                    cartDetail.setCart(cart);
+                    cartDetail.setProduct(reProduct);
+                    cartDetail.setQuantity(1);
+                    cartDetail.setPrice(reProduct.getPrice());
+                    this.cartDetailRepository.save(cartDetail);
+               }
+
+          }
+          // lưu cart_detail
      }
 }
